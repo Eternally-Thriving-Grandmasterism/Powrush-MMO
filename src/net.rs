6033ff410cb11651d233fce35773aced409032fa
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2025-12-27 00:46:50 -0500

    Create net.rs

---
+++ src/net.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+use bevy_replicon::prelude::*;
+
+pub struct MercyNetPlugin;
+
+impl Plugin for MercyNetPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(RepliconPlugins)
+            .replicate::<MercyPoints>()
+            .replicate::<TrustCredits>()
+            .replicate::<LatticeNode>();
+    }
+}
