5a754d65f02c759b6ffcb21e990933cfd351bad6
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2025-12-27 04:47:25 -0500

    Update net.rs

--- src/net.rs
+++ src/net.rs
@@ -5,12 +5,20 @@ pub struct MMONetPlugin;
 
 impl Plugin for MMONetPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(RepliconPlugins)
-            .replicate::<MercyPoints>()
-            .replicate::<TrustCredits>()
-            .replicate::<Inventory>()
-            .replicate::<Quest>()
-            .replicate::<CombatPlayer>()
-            .replicate::<Boss>();
+        app.add_plugins(RepliconPlugins.set(ServerPlugin {
+            tick_policy: TickPolicy::MaxTickRate(60),
+            visibility_policy: VisibilityPolicy::InterestManagement,
+            ..default()
+        }))
+        .add_plugins(RepliconPlugins.set(ClientPlugin {
+            tick_policy: TickPolicy::EveryFrame,
+            ..default()
+        }))
+        .replicate::<MercyPoints>()
+        .replicate::<TrustCredits>()
+        .replicate::<Inventory>()
+        .replicate::<Quest>()
+        .replicate::<CombatPlayer>()
+        .replicate::<Boss>();
     }
 }
