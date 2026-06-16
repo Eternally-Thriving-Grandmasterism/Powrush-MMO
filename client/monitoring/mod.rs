bd53bd5696403ba314117719d539572970a78e43
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-06-15 23:51:54 -0400

    Added Performance Profiler module and exports. Thunder locked in.

--- client/monitoring/mod.rs
+++ client/monitoring/mod.rs
@@ -1,6 +1,5 @@
 // client/monitoring/mod.rs
 // Ra-Thor Monitoring Module
-// Sovereign, modular monitoring & estimation systems
 
 pub mod localization;
 pub mod adaptive;
@@ -9,12 +8,14 @@ pub mod filters;
 pub mod safety_net;
 pub mod rbe_flow_responder;
 pub mod rbe_flow_dashboard_ui;
+pub mod performance_profiler;
 
-// Re-exports for convenience
+// Re-exports
 pub use localization::*;
 pub use adaptive::AdvancedAdaptiveLocalizer;
 pub use ensemble::LocalizedEnsembleKalmanFilter;
 pub use filters::{KalmanFilter1D, KalmanFilter2D, RTSFixedLagSmoother};
 pub use safety_net::{SafetyNetState, SafetyNetMonitoringSnapshot, SafetyNetMonitoringUpdate, RBEFlowAlert, RBEFlowDashboard, TimedRBEFlowAlert};
 pub use rbe_flow_responder::rbe_flow_responder_system;
-pub use rbe_flow_dashboard_ui::{spawn_rbe_flow_dashboard_ui, update_rbe_flow_dashboard};
+pub use rbe_flow_dashboard_ui::{spawn_rbe_flow_dashboard_ui, update_rbe_flow_dashboard, toggle_rbe_flow_dashboard};
+pub use performance_profiler::{spawn_performance_overlay, update_performance_overlay, toggle_performance_overlay};
