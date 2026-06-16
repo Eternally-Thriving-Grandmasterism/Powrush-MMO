40c81a97253c46102cf6351882bf70ab8b04ce51
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-06-15 22:00:03 -0400

    Exposed RBEFlowAlert, RBEFlowDashboard and responder system. Thunder locked in.

--- client/monitoring/mod.rs
+++ client/monitoring/mod.rs
@@ -7,10 +7,12 @@ pub mod adaptive;
 pub mod ensemble;
 pub mod filters;
 pub mod safety_net;
+pub mod rbe_flow_responder;
 
 // Re-exports for convenience
 pub use localization::*;
 pub use adaptive::AdvancedAdaptiveLocalizer;
 pub use ensemble::LocalizedEnsembleKalmanFilter;
 pub use filters::{KalmanFilter1D, KalmanFilter2D, RTSFixedLagSmoother};
-pub use safety_net::{SafetyNetState, SafetyNetMonitoringSnapshot, SafetyNetMonitoringUpdate};
+pub use safety_net::{SafetyNetState, SafetyNetMonitoringSnapshot, SafetyNetMonitoringUpdate, RBEFlowAlert, RBEFlowDashboard};
+pub use rbe_flow_responder::rbe_flow_responder_system;
