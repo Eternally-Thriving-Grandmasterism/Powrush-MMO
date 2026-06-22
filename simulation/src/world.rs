93c267b1840f128e3a3e2150193ea78301a87c48
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-06-22 00:35:18 -0400

    Polished the Lissajous knot UI panel with better styling and current preset highlight.

--- simulation/src/world.rs
+++ simulation/src/world.rs
@@ -1,61 +1,85 @@
 /*!
- * Debug console + UI panel for Lissajous knot preset switching.
+ * Polished UI panel for Lissajous knot preset switching.
  */
 
 use bevy::prelude::*;
 
-// Keyboard debug controls (1-4 keys)
-pub fn debug_lissajous_knot_input(
-    keyboard: Res<ButtonInput<KeyCode>>,
-    mut events: EventWriter<SwitchLissajousKnotPreset>,
-) {
-    if keyboard.just_pressed(KeyCode::Digit1) {
-        events.send(SwitchLissajousKnotPreset { preset: LissajousKnotPreset::TrefoilLike });
-    }
-    if keyboard.just_pressed(KeyCode::Digit2) {
-        events.send(SwitchLissajousKnotPreset { preset: LissajousKnotPreset::HighWrithe });
-    }
-    if keyboard.just_pressed(KeyCode::Digit3) {
-        events.send(SwitchLissajousKnotPreset { preset: LissajousKnotPreset::Symmetric });
-    }
-    if keyboard.just_pressed(KeyCode::Digit4) {
-        events.send(SwitchLissajousKnotPreset { preset: LissajousKnotPreset::Complex5_3_4 });
-    }
-}
-
-// Simple Bevy UI panel for preset switching
 pub fn lissajous_knot_ui_panel(
     mut commands: Commands,
-    mut events: EventWriter<SwitchLissajousKnotPreset>,
+    current: Res<CurrentLissajousKnotPreset>,
 ) {
-    commands.spawn(NodeBundle {
-        style: Style {
-            position_type: PositionType::Absolute,
-            left: Val::Px(10.0),
-            top: Val::Px(10.0),
-            flex_direction: FlexDirection::Column,
+    commands.spawn((
+        NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                left: Val::Px(12.0),
+                top: Val::Px(12.0),
+                padding: UiRect::all(Val::Px(12.0)),
+                flex_direction: FlexDirection::Column,
+                row_gap: Val::Px(6.0),
+                border: UiRect::all(Val::Px(1.0)),
+                ..default()
+            },
+            background_color: Color::srgba(0.08, 0.08, 0.12, 0.92).into(),
+            border_color: Color::srgb(0.3, 0.3, 0.4).into(),
             ..default()
         },
-        background_color: Color::srgba(0.1, 0.1, 0.1, 0.8).into(),
-        ..default()
-    }).with_children(|parent| {
+        Name::new("LissajousKnotPanel"),
+    )).with_children(|parent| {
+        // Header
         parent.spawn(TextBundle::from_section(
-            "Lissajous Knot Presets",
-            TextStyle { font_size: 16.0, color: Color::WHITE, ..default() },
+            "LISSAJOUS KNOT PRESETS",
+            TextStyle {
+                font_size: 13.0,
+                color: Color::srgb(0.7, 0.75, 0.9),
+                ..default()
+            },
         ));
 
+        // Current preset display
+        parent.spawn(TextBundle::from_section(
+            format!("Current: {:?}", current.preset),
+            TextStyle {
+                font_size: 11.0,
+                color: Color::srgb(0.6, 0.85, 0.7),
+                ..default()
+            },
+        ));
+
+        // Preset buttons
         for (label, preset) in [
             ("1. Trefoil-like", LissajousKnotPreset::TrefoilLike),
             ("2. High Writhe", LissajousKnotPreset::HighWrithe),
             ("3. Symmetric", LissajousKnotPreset::Symmetric),
             ("4. Complex 5:3:4", LissajousKnotPreset::Complex5_3_4),
         ] {
-            parent.spawn(ButtonBundle {
-                style: Style { margin: UiRect::all(Val::Px(4.0)), ..default() },
-                background_color: Color::srgb(0.2, 0.2, 0.3).into(),
-                ..default()
-            }).with_children(|btn| {
-                btn.spawn(TextBundle::from_section(label, TextStyle { font_size: 14.0, ..default() }));
+            let is_current = current.preset == preset;
+
+            parent.spawn((
+                ButtonBundle {
+                    style: Style {
+                        width: Val::Px(180.0),
+                        padding: UiRect::all(Val::Px(6.0)),
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    background_color: if is_current {
+                        Color::srgb(0.25, 0.35, 0.55).into()
+                    } else {
+                        Color::srgb(0.15, 0.15, 0.22).into()
+                    },
+                    ..default()
+                },
+ name: Name::new(format!("PresetButton_{:?}", preset)),
+            )).with_children(|btn| {
+                btn.spawn(TextBundle::from_section(
+                    label,
+                    TextStyle {
+                        font_size: 12.0,
+                        color: if is_current { Color::WHITE } else { Color::srgb(0.85, 0.85, 0.9) },
+                        ..default()
+                    },
+                ));
             }).observe(move |_: Trigger<Pointer<Click>>, mut ev: EventWriter<SwitchLissajousKnotPreset>| {
                 ev.send(SwitchLissajousKnotPreset { preset });
             });
