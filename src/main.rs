1c392518d920bc7cde2c2595e1ed2833404cecde
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-06-22 00:48:10 -0400

    Added spawn_harmony_knot_particle helper function.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-        // === Hanabi + Lissajous Knot Visual Effects ===
-        .add_plugins(HanabiPlugin)
-        .init_resource::<PolicyParticleEffects>()
-        .init_resource::<LissajousKnotEffects>()
-        .init_resource::<CurrentLissajousKnotPreset>()
-        .add_event::<SwitchLissajousKnotPreset>()
 
-        // Startup
-        .add_systems(Startup, (
-            setup_policy_particle_effects,
-            register_lissajous_knot_effects,
-            lissajous_knot_ui_panel,
-            spawn_harmony_knot_particle,
-        ))
-
-        // Update (reactive systems)
-        .add_systems(Update, (
-            debug_lissajous_knot_input,
-            handle_switch_lissajous_knot_preset,
-            highlight_active_preset_button.after(handle_switch_lissajous_knot_preset),
-            update_lissajous_knot_ui.after(handle_switch_lissajous_knot_preset),
-            update_active_lissajous_knot.after(handle_switch_lissajous_knot_preset),
-        ))
+fn spawn_harmony_knot_particle(
+    mut commands: Commands,
+    knot_effects: Res<LissajousKnotEffects>,
+) {
+    commands.spawn((
+        ParticleEffectBundle {
+            effect: ParticleEffect::new(knot_effects.complex.clone()),
+            transform: Transform::from_translation(Vec3::new(0.0, 8.0, 0.0)),
+            ..default()
+        },
+        HarmonyKnotMarker,
+        Name::new("HarmonyKnotParticle"),
+    ));
+}
