920b59297f687b247f4fc9b2cff5e33c24034405
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-06-22 01:06:41 -0400

    v19.4: Initialize ParticleVisualPool resource

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,10 +1,2 @@
-use simulation::{
-    // ... previous items ...
-    despawn_expired_visual_effects,  // NEW for lifetime control
-};
-
-// In build_app(), inside the particle systems group:
-.add_systems(Update, (
-    // ... existing reactive systems ...
-    despawn_expired_visual_effects,  // NEW
-));
+app.init_resource::<ParticleVisualPool>()
+       // ... other inits
