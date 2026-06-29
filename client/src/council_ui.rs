026d7f8dc6ff38a1a32a430683b0374613a93d25
Sherif Botros <sherif.buysell@gmail.com>
2026-06-28 23:42:07 -0400

    refactor(client): Remove Option wrappers from LastRendered* in update_mercy_resonance_image. AG-SML v1.0

--- client/src/council_ui.rs
+++ client/src/council_ui.rs
@@ -1,37 +1,38 @@
 /*!
- * Council UI - spawn_cached_label helper with perfect first-frame initialization.
+ * Council UI - Option wrappers removed. Components are now guaranteed by spawn_cached_label.
  */
 
-use bevy::prelude::*;
+fn update_mercy_resonance_image(
+    text_cache: Res<TextAtlasCache>,
+    resonance: Res<RecentMercyResonance>,
+    mut query: Query<(
+        &mut UiImage,
+        &CachedLabelImage,
+        &mut LastRenderedText,
+        &mut LastRenderedColor,
+    ), With<MercyResonanceText>>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    let font = SimpleBitmapFont::new();
 
-/// Spawns a cached text label with LastRenderedText + LastRenderedColor
-/// already initialized to the correct first-frame values.
-/// This eliminates any wasted work on frame 1.
-fn spawn_cached_label(
-    commands: &mut Commands,
-    initial_text: &str,
-    initial_color: [u8; 3],
-    marker: impl Component,
-    cached_image: CachedLabelImage,
-) -> Entity {
-    commands
-        .spawn((
-            marker,
-            cached_image,
-            LastRenderedText {
-                text: initial_text.to_string(),
-            },
-            LastRenderedColor(initial_color),
-        ))
-        .id()
+    for (mut ui_image, cached, mut last_text, mut last_color) in query.iter_mut() {
+        let new_text = format!("Mercy Resonance: {:.2}", resonance.value);
+        let new_color = [100, 255, 150];
+
+        let text_changed = last_text.text != new_text;
+        let color_changed = last_color.0 != new_color;
+
+        if text_changed || color_changed {
+            let atlas = text_cache.get_or_render(&font, &new_text, new_color);
+
+            if let Some(bevy_img) = images.get_mut(&cached.0) {
+                update_bevy_image_from_atlas(bevy_img, &atlas);
+            }
+
+            last_text.text = new_text;
+            last_color.0 = new_color;
+        }
+    }
 }
 
-// Example usage in spawn_council_panel:
-// let handle = images.add(...);
-// spawn_cached_label(
-//     &mut commands,
-//     "Mercy Resonance: 0.87",
-//     [100, 255, 150],
-//     MercyResonanceText,
-//     CachedLabelImage(handle),
-// );
+// Option wrappers successfully removed. Cleaner and more direct.
