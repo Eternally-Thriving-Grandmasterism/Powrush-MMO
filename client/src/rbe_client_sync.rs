d74d113c1d6b4e4c280b10cc23dbee6f3eb11b47
Sherif Botros <sherif.buysell@gmail.com>
2026-06-29 03:37:38 -0400

    refactor(client): Re-export gpu_simulation module for better modularity. AG-SML v1.0

--- client/src/rbe_client_sync.rs
+++ client/src/rbe_client_sync.rs
@@ -1,44 +1,11 @@
 /*!
- * Fully wired sync_gpu_simulation_state using the new resources
+ * RBE Client Sync
+ * 
+ * Re-exports the gpu_simulation module for better organization.
+ * Heavy logic has been moved to client/src/gpu_simulation/
  */
 
-pub fn sync_gpu_simulation_state(
-    mut gpu_state: ResMut<GpuSimulationState>,
-    time: Res<Time>,
-    mercy_resonance: Option<Res<RecentMercyResonance>>,
-    global_confidence: Option<Res<GlobalConfidence>>,
-    rbe_state: Option<Res<RbeGlobalState>>,
-    council_valence: Option<Res<CouncilValence>>,
-    // player_q: Query<(&Transform, &Velocity, &MercyAttunement), With<Player>>,
-) {
-    gpu_state.time = time.elapsed_seconds();
-    gpu_state.delta_time = time.delta_seconds();
+pub mod gpu_simulation;
 
-    if let Some(mercy) = mercy_resonance {
-        gpu_state.global_mercy_resonance = mercy.value;
-    }
-
-    if let Some(conf) = global_confidence {
-        gpu_state.global_confidence = conf.value;
-    }
-
-    if let Some(rbe) = rbe_state {
-        gpu_state.rbe_flow_rate = rbe.flow_rate;
-        gpu_state.total_rbe_circulating = rbe.total_circulating;
-        gpu_state.player_rbe_balance = rbe.player_balance;
-    }
-
-    if let Some(valence) = council_valence {
-        gpu_state.council_valence = valence.value;
-        gpu_state.active_council_action = valence.active_action;
-        gpu_state.council_participants = valence.participants;
-    }
-
-    // Player query example (uncomment when Player + Velocity components exist)
-    // if let Ok((transform, velocity, attunement)) = player_q.get_single() {
-    //     gpu_state.player_position = transform.translation.to_array();
-    //     gpu_state.player_velocity = velocity.0.to_array();
-    //     gpu_state.player_mercy_attunement = attunement.value;
-    //     gpu_state.player_thrivability = attunement.thrivability;
-    // }
-}
+// Re-export main types for convenience
+pub use gpu_simulation::*;
