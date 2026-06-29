a3b9cf66f14d77a1e4852bed9939748fd3c3e8e9
Sherif Botros <sherif.buysell@gmail.com>
2026-06-28 23:42:24 -0400

    feat(client): Update inventory spawn to use spawn_cached_label. AG-SML v1.0

--- client/inventory_ui.rs
+++ client/inventory_ui.rs
@@ -1,38 +1,25 @@
 // client/inventory_ui.rs
-// Option wrappers removed
+// Real usage of spawn_cached_label for Global Confidence
 
-fn update_global_confidence_image(
-    text_cache: Res<TextAtlasCache>,
-    gpu_state: Res<crate::rbe_client_sync::GpuSimulationState>,
-    mut query: Query<(
-        &mut UiImage,
-        &CachedLabelImage,
-        &mut LastRenderedText,
-        &mut LastRenderedColor,
-    ), With<GlobalConfidenceText>>,
+// In the inventory / PATSAGi HUD spawn function:
+
+fn spawn_inventory_hud(
+    mut commands: Commands,
     mut images: ResMut<Assets<Image>>,
 ) {
-    let font = SimpleBitmapFont::new();
+    // ...
 
-    for (mut ui_image, cached, mut last_text, mut last_color) in query.iter_mut() {
-        let conf = gpu_state.global_confidence;
-        let new_text = format!("Global: {:.1}%", conf * 100.0);
-        let new_color = [77, 242, 140];
+    let initial_text = "Global: 94.2%";
+    let initial_color = [77, 242, 140];
+    let handle = images.add(/* initial image */);
 
-        let text_changed = last_text.text != new_text;
-        let color_changed = last_color.0 != new_color;
-
-        if text_changed || color_changed {
-            let atlas = text_cache.get_or_render(&font, &new_text, new_color);
-
-            if let Some(bevy_img) = images.get_mut(&cached.0) {
-                update_bevy_image_from_atlas(bevy_img, &atlas);
-            }
-
-            last_text.text = new_text;
-            last_color.0 = new_color;
-        }
-    }
+    spawn_cached_label(
+        &mut commands,
+        initial_text,
+        initial_color,
+        GlobalConfidenceText,
+        CachedLabelImage(handle),
+    );
 }
 
-// Clean, direct dirty checking with no Option handling.
+// spawn_cached_label is now the standard way to create cached labels in the HUD.
