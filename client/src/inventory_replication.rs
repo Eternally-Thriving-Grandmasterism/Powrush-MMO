783a1d45e15f1f6496fe340ac0373714abccf64d
Sherif Botros <sherif.buysell@gmail.com>
2026-07-01 17:32:10 -0400

    UPDATE(client/src/inventory_replication.rs): receive_inventory_update now populates both ClientHotbar and ClientInventory from server. AG-SML v1.0 | TOLC 8. Thunder locked in. Yoi ⚡

--- client/src/inventory_replication.rs
+++ client/src/inventory_replication.rs
@@ -1,18 +1,39 @@
 /*!
  * client/src/inventory_replication.rs
- * Full general inventory support (ClientInventory resource).
+ * Full wiring of ClientInventory from server InventoryUpdate.
  */
 
 use bevy::prelude::*;
-use shared::protocol::HotbarSlot;
+use shared::protocol::{ServerMessage, HotbarSlot};
 
-/// Client-side full inventory (40 slots)
+#[derive(Resource, Default)]
+pub struct ClientHotbar {
+    pub slots: [HotbarSlot; 8],
+}
+
 #[derive(Resource, Default)]
 pub struct ClientInventory {
     pub slots: [HotbarSlot; 40],
 }
 
-// Existing ClientHotbar and receive_inventory_update remain
-// We will expand receive_inventory_update in next steps to also populate ClientInventory
+pub fn receive_inventory_update(
+    mut events: EventReader<crate::server_message_dispatcher::ServerMessageEvent>,
+    mut client_hotbar: ResMut<ClientHotbar>,
+    mut client_inventory: ResMut<ClientInventory>,
+    mut flash: ResMut<HotbarSyncFlash>,
+) {
+    for crate::server_message_dispatcher::ServerMessageEvent(msg) in events.read() {
+        if let ServerMessage::InventoryUpdate { hotbar, inventory, abundance_score, .. } = msg {
+            client_hotbar.slots = hotbar.clone();
+            client_inventory.slots = inventory.clone();
+
+            // Trigger hotbar flash
+            flash.timer.reset();
+            flash.timer.unpause();
+
+            info!("[InventoryReplication] Full inventory + hotbar synced | abundance={:.1}", abundance_score);
+        }
+    }
+}
 
 // End of inventory_replication.rs
